b71bba577417903a560c8d7bf23643bb2f6f54cf
Olympia Tsamou <63278430+olia92@users.noreply.github.com>
2021-09-28 14:23:33 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,82 @@
+# parallel-programming
+
+## Updateweights Serial
+
+```
+$ gcc -o updateweights updateweights-omp1.c
+
+$ time ./updateweights
+DONE!
+
+real    0m0,006s
+user    0m0,000s
+sys     0m0,007s
+
+```
+
+## OpenACC
+```
+$ nvc -acc -ta:tesla -o updateweights-acc updateweights-acc.c -Minfo=all
+main:
+     37, Generating create(X[:]) [if not already present]
+         Generating copyout(WL2[:][:]) [if not already present]
+         Generating create(DL2[:],DL1[:],OL1[:]) [if not already present]
+         Generating copyout(WL1[:][:]) [if not already present]
+Initialise_X:
+     56, Generating present(X[:])
+         Generating Tesla code
+         60, #pragma acc loop gang /* blockIdx.x */
+     62, Generating present(OL1[:])
+         Generating Tesla code
+         64, #pragma acc loop gang, vector(96) /* blockIdx.x threadIdx.x */
+     65, Generating present(DL1[:])
+         Generating Tesla code
+         69, #pragma acc loop gang, vector(96) /* blockIdx.x threadIdx.x */
+     70, Generating present(DL2[:])
+         Generating Tesla code
+         74, #pragma acc loop gang /* blockIdx.x */
+updateweight:
+     93, Generating present(delta[:n],input[:k],W[:n][:k+1])
+         Generating Tesla code
+         99, #pragma acc loop gang, vector(128) /* blockIdx.x threadIdx.x */
+        101, #pragma acc loop seq
+    101, Complex loop carried dependence of input->,W-> prevents parallelization
+
+$ time ./updateweights-acc 
+DONE!
+
+real    0m0,180s
+user    0m0,017s
+sys     0m0,145s
+```
+
+---
+
+## OpenMP
+
+### updateweights-omp1: parallel for
+
+```
+$ gcc -o updateweights-omp1 updateweights-omp1.c -fopenmp 
+$ time ./updateweights-omp1 
+DONE!
+
+real    0m0,021s
+user    0m0,046s
+sys     0m0,000s
+
+```
+
+### updateweights-omp2: sections
+
+```
+$ gcc -o updateweights-omp2 updateweights-omp2.c -fopenmp 
+
+$ time ./updateweights-omp2
+DONE!
+
+real    0m0,016s
+user    0m0,027s
+sys     0m0,004s
+
+```
---
+++ updateweights-omp2.c
@@ -0,0 +1,111 @@
+//*****************************
+/*
+    Neural Network
+    2 layers 
+*/
+//*****************************
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+#include "printing.h"
+
+// *******************************************************************
+// #pragma GCC optimize("O3","unroll-loops","omit-frame-pointer","inline", "unsafe-math-optimizations")
+// #pragma GCC option("arch=native","tune=native","no-zero-upper")
+//************************************************************
+
+#define NL1 100//hidden Layer
+#define NL2 10//Output
+#define N 12 //Input
+#define M 1//Number of inputs-outputs
+
+#define a 1
+
+//---------------------
+double X[N];
+// double Y[M][NL2]={0};
+double WL1[NL1][N+1]={0};
+double WL2[NL2][NL1+1]={0};
+double DL1[NL1];
+double OL1[NL1];
+double DL2[NL2];
+
+void Initialise_X();
+void Initialise_W();
+void updateweight(const int n,const int k,double W[n][k+1],double input[k],double delta[n]);
+
+
+int main(){
+    // SaveWeightsToFile("X_1.csv",M,N,X);
+    SaveWeightsToFile("W1_1.csv",NL1,N+1,WL1);
+    SaveWeightsToFile("W2_1.csv",NL2,NL1+1,WL2);
+
+
+    Initialise_X();
+    // Initialise_W();
+    // SaveWeightsToFile("X_2.csv",M,N,X);
+
+    updateweight(NL2,NL1,WL2,OL1,DL2);
+    updateweight(NL1,N,WL1,X,DL1);
+
+    SaveWeightsToFile("W1_2.csv",NL1,N+1,WL1);
+    SaveWeightsToFile("W2_2.csv",NL2,NL1+1,WL2);
+
+    printf("DONE!\n");
+    return 0;
+}
+
+//*********************************************************
+void Initialise_X(){
+#pragma omp sections
+{
+#pragma omp section 
+    for (int i=0;i<N;i++)
+        X[i]=i+1;
+
+#pragma omp section
+for (int i=0;i<NL1;i++)
+        OL1[i]=i+1;
+
+#pragma omp section
+    for (int i=0;i<NL1;i++)
+        DL1[i]=i+1;
+
+#pragma omp section
+    for (int i=0;i<NL2;i++)
+        DL2[i]=i+1;
+}
+}
+
+//*********************************************************
+
+// void Initialise_W(){
+// #pragma acc data create(WL1[:NL1][:N+1])
+// {
+// #pragma acc parallel loop present( WL1[:NL1][:N+1] )
+//     for (int j=0;j<=N;j++)
+//         for (int i=0;i<NL1;i++)
+//             WL1[i][j]=0;
+// }
+// }
+//*********************************************************
+
+
+void updateweight(const int n,const int k,double W[n][k+1],double input[k],double delta[n]){
+    // !!! W[n][k+1] !!!
+    //n : number of neurons output layer
+    //k : input layer
+// #pragma omp parallel for 
+    for(int i=0;i<n;i++){
+        double temp=delta[i];
+#pragma omp parallel for schedule(static,100)
+// #pragma omp parallel for schedule(dynamic)
+        for(int j=0;j<k;j++){
+            W[i][j] += a*temp*input[j]; 
+        }        
+        W[i][k] += a*temp;//bias update
+    }
+}
+
