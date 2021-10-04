fd71fbf6d8c787fa26d5c846fc9b8aedec562dbf
Olympia Tsamou <63278430+olia92@users.noreply.github.com>
2021-10-04 08:48:58 +0300

    Update printing.c

--- headers/printing.c
+++ headers/printing.c
@@ -3,3 +3,4 @@
 void Print1(int n,double a[n]);
 void Print(int n,int m,double a[][m]);
 void SaveWeightsToFile(char *fileName,int n,int m,double w[][m]);
+void SaveToFile(char *fileName,int n,double w[]);
